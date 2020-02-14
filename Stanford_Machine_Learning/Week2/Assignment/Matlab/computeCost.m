8fd91fa749d32c29778515349eb4be4efb64e21b
phonglm2 <51433705+phonglm2@users.noreply.github.com>
2020-02-14 09:21:09 +0900

    Update computeCost.m

--- Stanford_Machine_Learning/Week2/Assignment/Matlab/computeCost.m
+++ Stanford_Machine_Learning/Week2/Assignment/Matlab/computeCost.m
@@ -12,10 +12,12 @@ J = 0;
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
+H = [];
+deriative = [];
 
-
-
-
+H = theta(1, 1)*X(:, 1) + theta(2, 1)*X(:, 2);
+deriative = (H - y).^2;
+J = sum(deriative)/(2*m);
 
 % =========================================================================
 
