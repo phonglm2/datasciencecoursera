e9054e6f2bb5394d66e838c6ac33c68b1b8d4f03
phonglm2 <51433705+phonglm2@users.noreply.github.com>
2020-02-14 09:21:47 +0900

    Update computeCostMulti.m

--- Stanford_Machine_Learning/Week2/Assignment/Matlab/computeCostMulti.m
+++ Stanford_Machine_Learning/Week2/Assignment/Matlab/computeCostMulti.m
@@ -13,9 +13,8 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-
-
-
+s = power((X*theta - y), 2);
+J = sum(s)/(2*m);
 
 % =========================================================================
 
